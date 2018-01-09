e0b32b251eb16eedc7f9f8df27ee37407a293580
Philip Mannheimer <philip.mannheimer@gmail.com>
2018-01-09 16:30:19 +0100

    added dependency

--- pdfjam.rb
+++ pdfjam.rb
@@ -1,11 +1,10 @@
 class Pdfjam < Formula
   homepage "https://www2.warwick.ac.uk/fac/sci/statistics/staff/academic-research/firth/software/pdfjam"
-  url 
-"https://www2.warwick.ac.uk/fac/sci/statistics/staff/academic-research/firth/software/pdfjam/pdfjam_208.tgz"
+  url "https://www2.warwick.ac.uk/fac/sci/statistics/staff/academic-research/firth/software/pdfjam/pdfjam_208.tgz"
   sha256 "c731c598cfad076c985526ff89cbf34423a216101aa5e2d753a71de119ecc0f3"
   version "2.08"
 
- #depends_on :tex
+  depends_on :tex
 
   def install
     bin.install Dir["bin/*"]
