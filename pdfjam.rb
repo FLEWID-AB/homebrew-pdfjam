a61e22970cb36e0838718a34f2ca03137a01baed
Philip Mannheimer <philip.mannheimer@gmail.com>
2018-01-09 16:31:18 +0100

    removed dep

--- pdfjam.rb
+++ pdfjam.rb
@@ -4,8 +4,6 @@ class Pdfjam < Formula
   sha256 "c731c598cfad076c985526ff89cbf34423a216101aa5e2d753a71de119ecc0f3"
   version "2.08"
 
-  depends_on :tex
-
   def install
     bin.install Dir["bin/*"]
     man.install "man1"
